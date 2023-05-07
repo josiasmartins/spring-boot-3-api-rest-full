48935a0fb3456ac48a6a608cd97aef7f5f4bcc1b
Caitano <josiasmartinscaitano@gmail.com>
2023-05-07 12:25:23 -0300

    feat: spring security

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,36 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+//classe com as configurações de segurança da API
+@Configuration
+@EnableWebSecurity // usado personalizar as configurações de securança
+public class SecurityConfigurations {
+
+    @Bean // devolve um objeto
+    public SecurityFilterChain securityFilterChan(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().build();
+    }
+
+    @Bean // ensina como criar um objeto AuthenticationManager
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
